451afb156bca6c7e59910bfa69f31765a514aad0
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2023-12-21 22:23:43 +0500

    masalalar

---
+++ index.js
@@ -0,0 +1,159 @@
+// 1-masala
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// function sumArr(arg = 5) {
+//     let counter = 0;
+
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 1) {
+//             counter++;
+//         }
+//     }
+
+//     return counter;
+// }
+
+// let result = sumArr(arr);
+// console.log(result);
+
+
+
+
+
+// 2-masala
+
+
+
+
+// let arr = [1, 2, 3, 4, 14];
+
+// function sumArr(arg) {
+//     let sum = 1;
+
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 3 == 0 && arg[i] % 7 ==0) {
+//             sum = sum * arg[i];
+
+
+//         }
+
+
+//     }
+
+//     return sum;
+// }
+ 
+// console.log(sum);
+//  sumArr(arr);
+
+
+
+
+
+// 3-masala
+
+
+// let arr =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function toqson(arg){
+//     for(let i =0; i < arr; i++){
+//         if(arr[i] % 2 ==1){
+//            return arr[i] ;
+//         }
+        
+//     }
+// console.log(arr);
+
+// }
+
+// toqson(arr);
+
+
+
+
+
+
+// 4-masala
+
+ 
+
+// let arr =[1, 2, 3, 4, 5, 6, 7, 8, 9];
+// function array(arg){
+//     console.log( arr[1], arr[3], arr[5], arr[7]);
+// }
+
+// array();
+
+
+
+
+
+// 5-masala
+
+
+
+
+// let words = ["hello", "world", "javascript"];
+
+// function firstLetters(words) {
+//     let letters = [];
+//     for (let word of words) {
+//       letters.push(word[0]); 
+//     }
+//     return letters;
+//   }
+  
+//   firstLetters(words);
+//   console.log(words);
+
+
+
+
+// 6-masala
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// function sumArr(arg=11) {
+//     let counter = 0;
+
+//     for (let i = 1; i < arg.length; i++) {
+//         if (arg[i] % i == 0) {
+//             counter++;
+//         }
+//     }
+
+//     if(counter == 2){
+//         return "tub son emas"
+//     }else {
+//         return "tub son "
+
+//     }
+
+// }
+
+// let result = sumArr(arr);
+// console.log(result);
+
+
+
+
+
+
+// 7-masala
+
+// let arr=[10, 12, 13, 15, 16, 20];
+// function sonArr(arg=20){
+//     let counter;
+//     for (let i = 1; i < arg.length; i++) {
+//                 if (arg[i] % 2 == 0 && arg[i] % 5 ==0) {
+//                     counter++;
+//                 }
+//             }
+//             return counter;
+// }
+
+//   sonArr(arr);
+ 
